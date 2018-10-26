7b3f6efe2a583a696a5e689c10a3fe21731789db
Tyler Milner <tylermilner@gmail.com>
2018-10-26 13:43:50 -0500

    Initial Xcode project creation.

---
+++ ProjectStructureExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectStructureExample
+//
+//  Created by Tyler Milner on 10/26/18.
+//  Copyright © 2018 Tyler Milner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ProjectStructureExampleTests/ProjectStructureExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectStructureExampleTests.swift
+//  ProjectStructureExampleTests
+//
+//  Created by Tyler Milner on 10/26/18.
+//  Copyright © 2018 Tyler Milner. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectStructureExample
+
+class ProjectStructureExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
