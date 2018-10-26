487a9292c17d3f7cc2452965ee6f0d67e986933f
Tyler Milner <tylermilner@gmail.com>
2018-10-26 14:12:50 -0500

    Added "Kit" target.

---
+++ ProjectStructureExampleKit/ProjectStructureExampleKit.h
@@ -0,0 +1,19 @@
+//
+//  ProjectStructureExampleKit.h
+//  ProjectStructureExampleKit
+//
+//  Created by Tyler Milner on 10/26/18.
+//  Copyright © 2018 Tyler Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectStructureExampleKit.
+FOUNDATION_EXPORT double ProjectStructureExampleKitVersionNumber;
+
+//! Project version string for ProjectStructureExampleKit.
+FOUNDATION_EXPORT const unsigned char ProjectStructureExampleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectStructureExampleKit/PublicHeader.h>
+
+
